0d1b72103ca53170d4ac45b8eca852a2c0ac9bb8
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-07-08 22:49:09 +0300

    Додає часткове рішення 2 задачі

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle  from 'lodash.throttle';
+
+const iframeEl = document.querySelector('#vimeo-player')
+
+const options = {
+				id: 236203659,
+				width: 640,
+				loop: false
+			};
+const player = new Player(iframeEl.id, options);
+
+player.on('timeupdate', function(data) {
+   
+    duration: 61.857
+    percent: 0.049
+    seconds: 3.034  
+   
+ localStorage.setItem("videoplayer-current-time", data.seconds);
+
+});
+    
+const getStorage = localStorage.getItem("videoplayer-current-time");
+console.log(getStorage)
+
+player.setCurrentTime(getStorage).then(function(seconds) {
+  
+   seconds = getStorage;
+}).catch(function(error) {
+    switch (error.name) {
+      case 'RangeError':
+      break;
+
+      default:
+      break;
+    }
+});
 
