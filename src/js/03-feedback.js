b597f621453f5ecbbaeba475d857ddbe6cc481d5
Darya <97470059+DaryaBobur@users.noreply.github.com>
2022-07-10 16:19:42 +0300

    Додає рішення 3 задачі

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('input', throttle(formData, 1000));
+form.addEventListener('submit', onHandleSubmit);
+
+const readData = localStorage.getItem(LOCALSTORAGE_KEY);
+const parseData = JSON.parse(readData);
+try {
+          if (parseData) {
+    form.email.value = parseData.email;
+    form.message.value = parseData.message; 
+} else {
+    form.email.value = "";
+    form.message.value = ""; 
+}
+  } catch (error) {
+      console.log("parsing error");
+}
+
+function formData() {
+     const data = {
+        email: form.email.value,
+        message: form.message.value,
+     }
+ 
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data))
+}
+
+function onHandleSubmit(event) {
+    event.preventDefault();
+    
+     const {
+        elements: { email, message }
+    } = event.currentTarget;
+
+     const data = {
+        email: email.value,
+        message: message.value,
+     }
+    console.log(data);
+    
+   event.currentTarget.reset();
+   form.message.value = "";  
+   localStorage.clear();
+}
